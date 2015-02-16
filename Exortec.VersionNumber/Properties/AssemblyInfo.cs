d9781836e40f596f25b64603cf1511ba861b2da7
Sascha Andres <sascha.andres@outlook.com>
2015-02-16 20:20:01 +0100

    set version

--- Exortec.VersionNumber/Properties/AssemblyInfo.cs
+++ Exortec.VersionNumber/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
 [assembly: AssemblyProduct( "Exortec.VersionNumber" )]
-[assembly: AssemblyCopyright( "Copyright ©  2014" )]
+[assembly: AssemblyCopyright( "Copyright © Sascha Andres, 2015" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.2015.208" )]
-[assembly: AssemblyFileVersion( "1.0.2015.208" )]
+[assembly: AssemblyVersion( "1.1.2015.216" )]
+[assembly: AssemblyFileVersion( "1.1.2015.216" )]
